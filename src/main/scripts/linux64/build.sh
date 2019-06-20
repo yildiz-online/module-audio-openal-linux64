778b1c14b15ad28880b70d273bc3aa3a0b2d6e8b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-06-20 17:25:46 +0200

    [WIP] Code cleaning.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -34,4 +34,4 @@ rm -r ../../c++
 rm -r ../../includes
 rm -r ../linux64
 
-exit $r1
+exit ${r1}
--- src/main/scripts/linux64/build_sonar.sh
+++ src/main/scripts/linux64/build_sonar.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
@@ -8,4 +10,4 @@ rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 
-return $r1
+return ${r1}
